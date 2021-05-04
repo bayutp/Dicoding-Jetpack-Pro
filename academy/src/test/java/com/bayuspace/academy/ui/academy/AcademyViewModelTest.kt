24533a3fa41322352de338157246a93852432850
bayu <bayuteguhpamuji@gmail.com>
2021-05-04 15:52:09 +0700

    [bayu][academy] - add unit test

---
+++ academy/src/test/java/com/bayuspace/academy/ui/academy/AcademyViewModelTest.kt
@@ -0,0 +1,23 @@
+package com.bayuspace.academy.ui.academy
+
+import org.junit.Test
+
+import org.junit.Assert.*
+import org.junit.Before
+
+class AcademyViewModelTest {
+
+    private lateinit var viewModel: AcademyViewModel
+
+    @Before
+    fun setUp() {
+        viewModel = AcademyViewModel()
+    }
+
+    @Test
+    fun testGetCourses() {
+        val courseEntities = viewModel.getCourses()
+        assertNotNull(courseEntities)
+        assertEquals(5, courseEntities.size)
+    }
+}
