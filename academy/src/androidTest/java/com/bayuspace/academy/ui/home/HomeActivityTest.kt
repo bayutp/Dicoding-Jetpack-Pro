0903d36837c636a02a5f51f758f71f59af8a81a8
bayu <bayuteguhpamuji@gmail.com>
2021-05-05 15:29:58 +0700

    create instrumentation test

---
+++ academy/src/androidTest/java/com/bayuspace/academy/ui/home/HomeActivityTest.kt
@@ -0,0 +1,73 @@
+package com.bayuspace.academy.ui.home
+
+import androidx.recyclerview.widget.RecyclerView
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.contrib.RecyclerViewActions
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import com.bayuspace.academy.R
+import com.bayuspace.academy.utils.DataDummy
+import org.junit.Rule
+import org.junit.Test
+
+class HomeActivityTest {
+    private val dummyCourse = DataDummy.generateDummyCourse()
+
+    @get:Rule
+    var activityRule = ActivityScenarioRule(HomeActivity::class.java)
+
+    @Test
+    fun loadCourses() {
+        onView(withId(R.id.rv_academy)).check(matches(isDisplayed()))
+        onView(withId(R.id.rv_academy)).perform(
+            RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(
+                dummyCourse.size
+            )
+        )
+    }
+
+    @Test
+    fun loadDetailCourse() {
+        onView(withId(R.id.rv_academy)).perform(
+            RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(
+                0,
+                click()
+            )
+        )
+        onView(withId(R.id.text_title)).check(matches(isDisplayed()))
+        onView(withId(R.id.text_title)).check(matches(withText(dummyCourse[0].title)))
+        onView(withId(R.id.text_date)).check(matches(isDisplayed()))
+        onView(withId(R.id.text_date)).check(matches(withText("Deadline ${dummyCourse[0].deadline}")))
+    }
+
+    @Test
+    fun loadModule() {
+        onView(withId(R.id.rv_academy)).perform(
+            RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(
+                0,
+                click()
+            )
+        )
+        onView(withId(R.id.btn_start)).perform(click())
+        onView(withId(R.id.rv_module)).perform(
+            RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(
+                0,
+                click()
+            )
+        )
+        onView(withId(R.id.web_view)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun loadBookmark() {
+        onView(withText("Bookmark")).perform(click())
+        onView(withId(R.id.rv_bookmark)).check(matches(isDisplayed()))
+        onView(withId(R.id.rv_bookmark)).perform(
+            RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(
+                dummyCourse.size
+            )
+        )
+    }
+}
