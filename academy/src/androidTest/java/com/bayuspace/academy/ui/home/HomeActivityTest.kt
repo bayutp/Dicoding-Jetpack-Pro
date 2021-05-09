2183443cc0a4e0e60a8c81bc65169da309d9f803
bayu <bayuteguhpamuji@gmail.com>
2021-05-09 19:57:26 +0700

    implement iding resources

--- academy/src/androidTest/java/com/bayuspace/academy/ui/home/HomeActivityTest.kt
+++ academy/src/androidTest/java/com/bayuspace/academy/ui/home/HomeActivityTest.kt
@@ -1,7 +1,9 @@
 package com.bayuspace.academy.ui.home
 
 import androidx.recyclerview.widget.RecyclerView
+import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.IdlingRegistry
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.contrib.RecyclerViewActions
@@ -9,6 +11,9 @@ import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import com.bayuspace.academy.R
 import com.bayuspace.academy.utils.DataDummy
+import com.bayuspace.academy.utils.EspressoIdlingResources
+import org.junit.After
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 
@@ -18,9 +23,20 @@ class HomeActivityTest {
     @get:Rule
     var activityRule = ActivityScenarioRule(HomeActivity::class.java)
 
+    @Before
+    fun setup() {
+        ActivityScenario.launch(HomeActivity::class.java)
+        IdlingRegistry.getInstance().register(EspressoIdlingResources.idlingResource)
+    }
+
+    @After
+    fun tearDown() {
+        IdlingRegistry.getInstance().unregister(EspressoIdlingResources.idlingResource)
+    }
+
     @Test
     fun loadCourses() {
-        delay()
+        //delay()
         onView(withId(R.id.rv_academy)).check(matches(isDisplayed()))
         onView(withId(R.id.rv_academy)).perform(
             RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(
@@ -31,14 +47,14 @@ class HomeActivityTest {
 
     @Test
     fun loadDetailCourse() {
-        delay()
+        //delay()
         onView(withId(R.id.rv_academy)).perform(
             RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(
                 0,
                 click()
             )
         )
-        delay()
+        //delay()
         onView(withId(R.id.text_title)).check(matches(isDisplayed()))
         onView(withId(R.id.text_title)).check(matches(withText(dummyCourse[0].title)))
         onView(withId(R.id.text_date)).check(matches(isDisplayed()))
@@ -47,30 +63,30 @@ class HomeActivityTest {
 
     @Test
     fun loadModule() {
-        delay()
+        //delay()
         onView(withId(R.id.rv_academy)).perform(
             RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(
                 0,
                 click()
             )
         )
-        delay()
+        //delay()
         onView(withId(R.id.btn_start)).perform(click())
-        delay()
+        //delay()
         onView(withId(R.id.rv_module)).perform(
             RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(
                 0,
                 click()
             )
         )
-        delay()
+        //delay()
         onView(withId(R.id.web_view)).check(matches(isDisplayed()))
     }
 
     @Test
     fun loadBookmark() {
         onView(withText("Bookmark")).perform(click())
-        delay()
+        //delay()
         onView(withId(R.id.rv_bookmark)).check(matches(isDisplayed()))
         onView(withId(R.id.rv_bookmark)).perform(
             RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(
@@ -79,15 +95,15 @@ class HomeActivityTest {
         )
     }
 
-    private fun delay() {
+    /*private fun delay() {
         try {
             Thread.sleep(SERVICE_LATENCY_IN_MILLIS)
         } catch (e: InterruptedException) {
             e.printStackTrace()
         }
-    }
+    }*/
 
-    companion object{
+    companion object {
         private const val SERVICE_LATENCY_IN_MILLIS = 3_000L
     }
 }
